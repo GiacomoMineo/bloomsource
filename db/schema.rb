ebe4b8fcf5bc95848aae4d19ba2d373008b9c911
Giacomo Mineo <giacomo.mineo91@gmail.com>
2016-03-14 16:35:45 +0100

    Changed categories landing style

--- db/schema.rb
+++ db/schema.rb
@@ -77,6 +77,12 @@ ActiveRecord::Schema.define(version: 20160314111344) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "informers", force: :cascade do |t|
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "invitations", force: :cascade do |t|
     t.integer  "creator_id"
     t.integer  "library_id"
