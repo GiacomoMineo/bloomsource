2e3e964ae461a4d3af0f64eda08bdc24cc0f1378
PvtMoser <wicinovsky@gmail.com>
2016-03-15 11:10:28 +0100

    Adjust feedback mailer

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,5 +1,5 @@
 class FeedbackMailer < ApplicationMailer
-	default :to => "coilbeeapp@gmail.com"
+	default :to => "getbloomsource@gmail.com"
 	def send_feedback(feedback)
 		@feedback = feedback
 		mail from: @feedback.email, subject: @feedback.subject, body: @feedback.content
